9274c441fe932b3be305ec263275554844dfd0b3
letungbaoan <baoan7604@gmail.com>
2026-03-23 00:45:55 +0700

    Add UMP and PNT

--- lab1/data/daihoc.js
+++ lab1/data/daihoc.js
@@ -17,6 +17,24 @@ const DAIHOC = [
     faculties: "Kỹ thuật - Kinh tế",
     students: 8000,
   },
+  {
+    longitude: 106.663,
+    latitude: 10.7548,
+    symbol_url: "ump.jpg",
+    name: "Đại Học Y Dược Thành Phố Hồ Chí Minh (UMP)",
+    founded: 1947,
+    faculties: "Y Dược",
+    students: 15000,
+  },
+    {
+    longitude: 106.6658946,
+    latitude: 10.7735398,
+    symbol_url: "pnt.jpg",
+    name: "Trường Đại học Y khoa Phạm Ngọc Thạch (PNT)",
+    founded: 1989,
+    faculties: "Y Dược",
+    students: 10000,
+  },
 ];
 
 export default DAIHOC;
