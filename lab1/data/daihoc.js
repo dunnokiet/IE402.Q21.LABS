3de7c40abe3d8cdffc8de187a458591415735301
dunnokiet <tianhaivn@gmail.com>
2026-03-22 22:54:51 +0700

    feat: init lab1

---
+++ lab1/data/daihoc.js
@@ -0,0 +1,22 @@
+const DAIHOC = [
+  {
+    longitude: 106.8004792,
+    latitude: 10.8700142,
+    symbol_url: "uit.png",
+    name: "ĐH Công nghệ Thông tin TP.HCM (UIT)",
+    founded: 2006,
+    faculties: "Kỹ thuật - Công nghệ",
+    students: 40000,
+  },
+  {
+    longitude: 107.0821903,
+    latitude: 10.3485815,
+    symbol_url: "bvu.png",
+    name: "ĐH Bà Rịa - Vũng Tàu (BVU)",
+    founded: 2006,
+    faculties: "Kỹ thuật - Kinh tế",
+    students: 8000,
+  },
+];
+
+export default DAIHOC;
---
+++ lab1/data/index.js
@@ -0,0 +1,6 @@
+import TAYNINH from "./nambo/tayninh.js";
+
+export { default as DUONG } from "./duong.js";
+export { default as DAIHOC } from "./daihoc.js";
+
+export const NAMBO = [TAYNINH];
